ea693fa3e2c633638f824052ff695cf00e54cd85
Radhaji Mani <radhaji.mani@gmail.com>
2015-02-26 06:17:00 +0000

    Header updates

--- sensorhubdemoclient/js/weather_wind_direction.js
+++ sensorhubdemoclient/js/weather_wind_direction.js
@@ -1,6 +1,14 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the License.
+ *
+ * The Initial Developer is Terravolv, Inc. Portions created by the Initial
+ * Developer are Copyright (C) 2014-2015 the Initial Developer. All Rights Reserved.
+ */
  
  $(function () {
     
