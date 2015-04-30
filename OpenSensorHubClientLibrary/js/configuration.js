53879245a7d444ac93f8fba160ba51b04d2ef84c
Radhaji Mani <radhaji.mani@gmail.com>
2015-04-29 21:46:46 -0500

    Reset Data Items

--- OpenSensorHubClientLibrary/js/configuration.js
+++ OpenSensorHubClientLibrary/js/configuration.js
@@ -18,7 +18,6 @@
     addserverdialog.dialog( "open" );
   });  
 
-
   var addserverdialog = $( "#newserverdialog" ).dialog({
     autoOpen: false,
     modal: true,
@@ -137,6 +136,268 @@
        alert(e);
      }
     }
-  });  
-});
+  });
 
+  var confirmdialog = $( "#data-items-reset-dialog-confirm").dialog({
+    autoOpen: false,
+    resizable: false,
+    modal: true,
+    buttons: [
+      {
+        text: "Yes",
+        click: function () {
+                  $(this).dialog('close');
+                  resetDataItems(true);
+                 }
+      },
+      {
+        text: "No",
+        click: function () {
+                  $(this).dialog('close');
+                  resetDataItems(false);
+                }
+      }
+    ]
+  });
+  
+  function resetDataItems(confirmed) {
+    if (confirmed) {
+      loadDataItems();
+      $("#tree").jstree('refresh');
+    }
+  }
+  
+  function loadDataItems(dataItems) {
+
+    if ((undefined === dataItems) || (null === dataItems)) {
+			// Add root data item node if no data items found
+			dataItems = '[{"id":"440ecdb0-d2a9-ea62-37de-fe78c30b3b1e","text":"Data Items","icon":true,"li_attr":{"id":"440ecdb0-d2a9-ea62-37de-fe78c30b3b1e"},"a_attr":{"href":"#","id":"440ecdb0-d2a9-ea62-37de-fe78c30b3b1e_anchor"},"state":{"loaded":true,"opened":true,"selected":false,"disabled":false},"data":{"0":{"key":"created","value":"' + moment().format() + '"}},"children":[]}]';
+			localStorage.setItem("sosDataItems",dataItems);
+		} 
+		
+		var jsTreeFormat = JSON.parse(dataItems, function(k, v) {
+			if (k === "parentid") {
+				this.parent = (v) ? v : '#';
+			} else if (k === "name") {
+				this.text = v;
+			} else {
+				return v;
+			}
+		});
+
+		$('#tree').jstree(  { 'core' : {'check_callback' :  function (op, node, par, pos, more) {
+                                                          if ( more && more.dnd && (op === 'move_node' || op === 'copy_node') && (par.id === "#")) {
+                                                            // Don't allow moving before the root node (Data Items)
+                                                            return false;
+                                                          }
+                                                          return true;
+                                                        },
+                                    'multiple' : false,
+                                    'data' : jsTreeFormat }, 
+                          'dnd': { check_while_dragging: true /* You need this or check_callback will not be called when moving nodes */},                          
+                          'plugins' : ['contextmenu','dnd'],
+                          'contextmenu' : { 'items' : function(node) {
+                                                        var itm = $.jstree.defaults.contextmenu.items();
+                                                        delete itm.create.action;
+                                                        itm.create.label = "New Item";
+                                                        itm.rename.label = "Rename Item";
+                                                        itm.remove.label = "Delete Item";
+                                                        itm.ccp.label = "Edit Item";
+                                                        itm.create.action = function (data) {
+                                                                              var inst = $.jstree.reference(data.reference),
+                                                                                  obj = inst.get_node(data.reference);
+                                                                              inst.create_node( obj, 
+                                                                                                { type : "default", 
+                                                                                                  id: Math.uuid().toLowerCase(),
+                                                                                                  data : { "0" : {"key"   : "created",
+                                                                                                                  "value" : moment().format() },
+                                                                                                           "1" : {"key"   : "parentid",
+                                                                                                                  "value" : obj.id },
+                                                                                                           "2" : {"key"   : "moved",
+                                                                                                                  "value" : null },
+                                                                                                           "3" : {"key"   : "oldparentid",
+                                                                                                                  "value" : null },
+                                                                                                           "4" : { "key"  : "observables",
+                                                                                                                   "value" : {} },
+                                                                                                           "5" : { "key"  : "actions",
+                                                                                                                   "value" : {} } }    
+                                                                                                }, 
+                                                                                                "last", 
+                                                                                                function (new_node) {
+                                                                                                  // Allow to edit the name of new node    
+                                                                                                  setTimeout(function () { inst.edit(new_node); },0);
+                                                                                                }
+                                                                                              )
+                                                                            };
+                                                        itm["actions"] =  { label: "Assign Actions",
+                                                                            action: function (node) {
+                                                                                      instNode = $.jstree.reference(node.reference).get_node(node.reference); 
+                                                                                      addactionsdialog.data('node', instNode).dialog( "open" );                                                                            
+                                                                                    }
+                                                                          };  
+                
+                                                        return itm;
+                                                      }
+                                          },
+                        } );
+  }
+
+  var addactionsdialog = $( "#assignactionsdialog" ).dialog({
+    autoOpen: false,
+    modal: true,
+    buttons: {
+      Assign: function() {
+        assignAction($(this).data("node").data);
+        $( this ).dialog( "close" );
+      },
+      Cancel: function() {
+        $( this ).dialog( "close" );
+      }
+    },
+    close: function() {
+        addactionsform[ 0 ].reset();
+    }
+  });
+  
+  var addactionsform = addactionsdialog.find( "form" ).submit(function( event ) {
+    addactionsdialog.dialog( "close" );
+    event.preventDefault();
+  });   
+
+  function assignAction(nData) {
+
+    var selectedAction = $("#selectaction option:selected").val(),
+        actions = null;
+    
+    if ("" === selectedAction) {
+      alert("Please select an action.");
+      return;
+    }
+
+    // Get data item's actions
+    for (var i in nData) {
+      // Just in case some other code touched my collection object, check hasOwnProperty!
+      if (nData.hasOwnProperty(i)) { 
+        switch (nData[i]['key']) {
+          case "actions":
+            actions = nData[i]['value'];
+            break;
+        }
+      }
+    }
+
+    // TODO: Only one default action check needs to be added here
+    for (var i in actions) {
+      // Just in case some other code touched my collection object, check hasOwnProperty!
+      if (actions.hasOwnProperty(i)) {
+        if (selectedAction === actions[i]['name']) {
+          alert("Action is already assigned to data item.");
+          return;
+        }        
+      }
+    }
+    
+    // Tag selected action with the data item.
+    actions[Object.keys(actions).length+1] =  { name: selectedAction, 
+                                                isdefault: ($('#isdefaultaction').prop('checked')) ? 1 : 0, 
+                                                id: Math.uuid().toLowerCase()};
+                                                      
+  } // assignAction()
+
+  $('#tree').on('move_node.jstree', function(e, data) {
+    // Extract into separate variables for ease of following
+    var oldParentId = data.old_parent;
+    var newParentId = data.parent;
+    var me = data.node;  // This is me!  I just moved!
+    var mydata = me.data; // Get my collection of objects that have <key, value> pairs.
+    for (var i in mydata) {
+      // Just in case some other code touched my collection object, check hasOwnProperty!
+      if (mydata.hasOwnProperty(i)) { 
+        switch (mydata[i]['key']) {
+          case "moved":
+            mydata[i]['value'] = moment().format();
+            break;
+          case "parentid":
+            mydata[i]['value'] = newParentId;
+            break;
+          case "oldparentid":
+            mydata[i]['value'] = oldParentId;
+            break;
+        }
+      }
+    }
+  });
+  
+  $('#tree').on('hover_node.jstree',function(e,data){
+    if ($('#showDataItemDetails').prop('checked'))
+      $('a[id=' + data.node.id + '_anchor]').webuiPopover({title:data.node.text,content:'Content'});
+    else
+      $('a[id=' + data.node.id + '_anchor]').webuiPopover('destroy');
+  })    
+
+  $("#btnAssociateDataItem").button().click(function(){
+
+    if (undefined === $("#offIdentifier").val().trim() || !$("#offIdentifier").val().trim()) {
+      alert( "Identifier must be set." );
+      return;
+    }
+
+    if (undefined === $("#grtemporalfilter").val().trim() || !$("#grtemporalfilter").val().trim()) {
+      alert( "Time filter must be supplied." );
+      return;
+    }
+
+    var templateurl = S($("#offServers option:selected").val())
+                  .getresulttemplateurl($("#offIdentifier").val(), 
+                                        $("#offObservables option:selected").text());
+    
+    var wsurl = S($("#offServers option:selected").val())
+              .getresultwsurl($("#offIdentifier").val(), 
+                              $("#offObservables option:selected").text(),
+                              $("#grtemporalfilter").val());
+    
+    // Get the select data item from tree
+    var selectedNode= $('#tree').jstree(true).get_selected(true);
+
+    var mydata = selectedNode[0].data; // Get my collection of objects that have <key, value> pairs.
+
+    for (var i in mydata) {
+
+      // Just in case some other code touched my collection object, check hasOwnProperty!
+      if (mydata.hasOwnProperty(i)) {
+
+        if (mydata[i]['key'] === "observables") {
+        
+          if (null === mydata[i]['value']) 
+            mydata[i]['value'] = {};
+
+          // Save the template URL and feed URL for the current observable
+          var observableProperty = new Object();
+          observableProperty.tempateurl = templateurl;
+          observableProperty.dataurl = wsurl;
+          mydata[i]['value'][Object.keys(mydata[i]['value']).length+1] =  observableProperty;
+          
+          break;
+        }        
+      }
+    }
+    alert('Observed property successfully associated with selected data item.');
+  });
+  
+  $("#btnSaveTree").button().click(function(){
+    var _DI = JSON.stringify($("#tree").jstree(true).get_json());
+    localStorage.setItem("sosDataItems",_DI);
+    alert("Data items saved successfully.")
+  });
+
+  $("#btnResetTree").button().click(function(){
+    confirmdialog.dialog( "open" );
+  });
+
+	if("undefined" !== typeof(Storage) ) {
+		loadDataItems(localStorage.getItem("sosDataItems"));
+	} else {
+    throw new Error("Local storage not supported.");
+	}  
+  
+});
